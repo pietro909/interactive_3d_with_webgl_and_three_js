d9c52d617308bac8bbb4ca5b96d39e400057542b
pietroTP <pietro.grandi.3d@gmail.com>
2015-10-06 16:10:43 +0200

    demo slightly improved, let's work on it later tonight

--- demo/demo.js
+++ demo/demo.js
@@ -2,26 +2,50 @@ window.setTimeout(function onLoad() {
     'use strict';
 
     var
+    // basic setup
     scene, camera, renderer, container, material, geometry, mesh,
-    theLight1, theLight2, ambientLight, orbitControls, mouseVector, rayCaster,
-    lastIntersected, highlightMaterial, physijsMaterial,
+    // lights
+    theLight1, theLight2, ambientLight,
+    // user interaction
+    orbitControls, mouseVector, rayCaster,
+    lastIntersected, highlightMaterial,
+    // collisions
     planeGeometry, planeMaterial, planePhysiMaterial, planeMesh,
+    // environment
     envSphereGeometry, envSphereText, envSphereMaterial, envSphere,
-    cubeCam,
-    meshAndCameras = [],
-    pos_x, pos_y, pos_z,
-    scale = 1, side = 3,
+    cubeCam, meshAndCameras = [],
+    // animation
+    direction = 4,
+    // render setup
     render_width = 640, render_height = 480,
+    // dependencies here
     THREE = require('three'),
     // just a side-effect
     ColladaLoader = require('three-loaders-collada')(THREE),
     Physijs = require('physijs-browserify')(THREE),
     OrbitControls = require('three-orbit-controls')(THREE);
+
+    Physijs.scripts.worker = '../node_modules/physijs-browserify/libs/physi-worker.js';
+    Physijs.scripts.ammo = 'ammo.js';
     
-    container = document.getElementById('theContainer');
+    container = document.getElementById( 'theContainer' );
 
     scene = new Physijs.Scene();
-    //scene.setGravity( new THREE.Vector3( 0, -30, 0));
+    scene.setGravity( new THREE.Vector3( 0, -6, 0) );
+
+    // let it dance in the air...
+    setInterval(
+        function( timeframe )
+        {
+            direction *= -1;
+            var x = Math.random() * direction;
+            var y = Math.random() * direction;
+            scene.setGravity(
+                new THREE.Vector3(x, y, 0)
+            );
+        },
+        2000
+    );
     
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xaabbff);
@@ -30,7 +54,7 @@ window.setTimeout(function onLoad() {
     container.appendChild(renderer.domElement);
     
     camera = new THREE.PerspectiveCamera(50, render_width / render_height, 1, 20000);
-    camera.position.set( 0, 0, 10 );
+    camera.position.set( 0, 0, 200 );
 
     orbitControls = new OrbitControls( camera );
 
@@ -47,7 +71,6 @@ window.setTimeout(function onLoad() {
     theLight2 = new THREE.DirectionalLight(0xddddff, 0.7);
     theLight2.position.set(10, 10, -20);
     scene.add(theLight2);
-
 
     // environment map
     envSphereGeometry = new THREE.SphereGeometry(5000, 64, 64);
@@ -75,8 +98,6 @@ window.setTimeout(function onLoad() {
         reflectivity: 0.9
     });
 
-    physijsMaterial = Physijs.createMaterial( material, 0.3, 0.8 );
-
     meshAndCameras.push( cubeCam );
 
     var loader = new THREE.ColladaLoader();
@@ -84,16 +105,29 @@ window.setTimeout(function onLoad() {
         '../assets/3d/robot08.dae',
         function( model )
         {
+            // 7 letters
+            var x = 4;
+
             model.scene.traverse(
                 function( node )
                 {
-                    var mesh;
+                    var mesh, phyMat;
                     
                     if ( node instanceof THREE.Mesh )
                     {
-                        mesh = new Physijs.BoxMesh( node.geometry, material );
-                        mesh.scale.set( 100, 100, 100 );
-                        mesh.__dirtyScale = true;
+                        phyMat = Physijs.createMaterial(
+                            material,
+                            0.3,
+                            0.8
+                        );
+                        node.geometry.computeBoundingBox();
+                        mesh = new Physijs.BoxMesh(
+                            node.geometry,
+                            phyMat
+                        );
+                        mesh.position.setX( 14 * x );
+                        mesh.__dirtyPosition = true;
+                        x -= 1;
                         scene.add( mesh );
                     }
                 }
@@ -110,7 +144,7 @@ window.setTimeout(function onLoad() {
             meshAndCameras[i].updateCubeMap( renderer, scene );
         }
 
-//        scene.simulate();
+        scene.simulate();
         
         renderer.render( scene, camera );
 
